0e6c194cba734ed241958940002fd69ba2d401a5
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-08-04 16:44:25 -0700

    Shows me changing the name Header

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -27,7 +27,7 @@ const Sidebar = () => {
   return (
     <>
       <div className='md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]'>
-        <img src={logo} alt={logo} className='w-full h-14 object-contain' />
+        <h1 className='font-bold text-4xl text-purple-700 text-left drop-shadow-2xl-4xl-white'>Moopsify</h1>
         <NavLinks />
       </div>
       <div className='absolute md:hidden block top-6 right-3'> 
@@ -36,7 +36,7 @@ const Sidebar = () => {
         ): <HiOutlineMenu className='w-6 h-6 text-white mr-2' onClick={() => setMobileMenuOpen(true)}/>}
       </div>
       <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
-        <img src={logo} alt={logo} className='w-full h-14 object-contain' />
+      <h1 className='font-bold text-4xl text-purple-700 text-left drop-shadow-lg'>Moopsify</h1>
         <NavLinks handleClick={() => setMobileMenuOpen(false)}/>
       </div>
     </>
