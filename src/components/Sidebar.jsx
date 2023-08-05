38f36a387ad437efe4e794378195085736128ff5
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-08-04 17:49:37 -0700

    shows the song details page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ const NavLinks = ({ handleClick}) => (
       <NavLink 
       key={link.name}
       to={link.to}
-      className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+      className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-[#e0aaff]"
       onClick={() => handleClick && handleClick()}>
         <link.icon className="w-6 h-6 mr-2"/>
         {link.name}
@@ -26,8 +26,8 @@ const Sidebar = () => {
 
   return (
     <>
-      <div className='md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]'>
-        <h1 className='font-bold text-4xl text-purple-700 text-left drop-shadow-2xl-4xl-white'>Moopsify</h1>
+      <div className='md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#10002b]'>
+        <h1 className='font-bold text-4xl text-[#9d4edd] text-left drop-shadow-2xl-4xl-white'>Moopsify</h1>
         <NavLinks />
       </div>
       <div className='absolute md:hidden block top-6 right-3'> 
@@ -35,7 +35,7 @@ const Sidebar = () => {
           <RiCloseLine className='w-6 h-6 text-white mr-2' onClick={() => setMobileMenuOpen(false)}/>
         ): <HiOutlineMenu className='w-6 h-6 text-white mr-2' onClick={() => setMobileMenuOpen(true)}/>}
       </div>
-      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#3c096c] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
       <h1 className='font-bold text-4xl text-purple-700 text-left drop-shadow-lg'>Moopsify</h1>
         <NavLinks handleClick={() => setMobileMenuOpen(false)}/>
       </div>
