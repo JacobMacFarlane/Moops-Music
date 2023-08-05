ccc1c9cb1817c20d313182442756ca2108ffbb45
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-08-04 19:33:04 -0700

    feat-shows the around you page

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -15,10 +15,12 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
             getSongDetails: builder.query({ query: ({ songid }) => `/v1/tracks/details?track_id=${songid}`}),
             getSongRelated: builder.query({ query: ({ songid }) => `/v1/tracks/related?track_id=${songid}`}),
             getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
+            getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}` }),
         }),
     })
 
     export const {
+        useGetSongsByCountryQuery,
         useGetTopChartsQuery,
         useGetSongDetailsQuery,
         useGetSongRelatedQuery,
