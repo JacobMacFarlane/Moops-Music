e2bbebf9b9441cf7aa47524d297b75916d30cf69
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-08-04 19:17:28 -0700

    shows the related songs in each artists view

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
     export const shazamCoreApi = createApi({
         reducerPath: 'shazamCoreApi',
         baseQuery: fetchBaseQuery({
-            baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+            baseUrl: 'https://shazam-core.p.rapidapi.com',
             prepareHeaders: (headers) => {
                 headers.set('X-RapidAPI-Key', '1076e34188mshefd8ed48460a24cp1bbe33jsnb9eb5ceb8692');
 
@@ -11,9 +11,10 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
             },
         }),
         endpoints: (builder) => ({
-            getTopCharts: builder.query({ query: () => '/charts/world' }),
-            getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}`}),
-            getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}`})
+            getTopCharts: builder.query({ query: () => '/v1/charts/world' }),
+            getSongDetails: builder.query({ query: ({ songid }) => `/v1/tracks/details?track_id=${songid}`}),
+            getSongRelated: builder.query({ query: ({ songid }) => `/v1/tracks/related?track_id=${songid}`}),
+            getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
         }),
     })
 
@@ -21,4 +22,5 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
         useGetTopChartsQuery,
         useGetSongDetailsQuery,
         useGetSongRelatedQuery,
+        useGetArtistDetailsQuery
     } = shazamCoreApi
